c3f62b2acfe42c004fbcce8ece69060adfe11116
Mohammad Namakshenas <51627278+namakshenas@users.noreply.github.com>
2022-01-23 23:10:25 +0330

    Update opt.js

--- opt.js
+++ opt.js
@@ -3,9 +3,13 @@ obj = function (x) {
   return (x[0]-1.0)*(x[0]-1.0) + (x[1]+0.5)*(x[1]+0.5) + (x[2]+2.0)*(x[2]+2.0)
 }
 
+
+// initial values
 var x0 = [1.2, 2.0, -3.1]
 
+
+// Solve!
 var solution = optimjs.minimize_Powell(obj, x0)
 
-console.log(solution)
+
 alert("variables = " + solution.argument + "\n\n objective = " + solution.fncvalue)
